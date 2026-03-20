57e373299074dda406d58bcbfb7847e3fe515c5b
sochima-ifedikwa <ifedikwasochima@gmail.com>
2026-03-20 01:19:38 -0500

    Add MovieGenreViewModel to manage movie and genre data for views

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -18,17 +18,19 @@ namespace MvcMovie.Controllers
         {
             _context = context;
         }
-
-        // GET: Movies
-        [HttpPost]
         
-        public async Task<IActionResult> Index(string searchString)
+        // GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
             if (_context.Movie == null)
             {
                 return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
             }
 
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
             var movies = from m in _context.Movie
                          select m;
 
@@ -37,8 +39,18 @@ namespace MvcMovie.Controllers
                 movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
             }
 
-            return View(await movies.ToListAsync());
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
 
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
@@ -149,6 +161,12 @@ namespace MvcMovie.Controllers
 
             return View(movie);
         }
+
+        // [HttpPost]
+        // public string Index(string searchString, bool notUsed)
+        // {
+        //     return "From [HttpPost]Index: filter on " + searchString;
+        // }
 
         // POST: Movies/Delete/5
         [HttpPost, ActionName("Delete")]
--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c0c5bcd2e0347b72d29320a82db73f9caa6e985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f07fb24bada150dd72b8a190ac00edaf563fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
